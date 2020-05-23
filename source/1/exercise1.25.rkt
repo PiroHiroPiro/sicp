1947ab5dbfb1280d2a387cba8918b493f45ccc32
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-05-23 11:51:35 +0900

    :construction: WIP

---
+++ source/1/exercise1.25.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+(require "../utils/common.rkt")
+
+(define (runtime) (current-inexact-milliseconds))
+
+(define (fast-expt b n)
+  (define (fast-expt-iter b n a)
+    (cond
+      ((= n 0) a)
+      ((even? n) (fast-expt-iter (square b) (halve n) a))
+      (else (fast-expt-iter b (- n 1) (* a b)))
+    )
+  )
+  (fast-expt-iter b n 1)
+)
+
+(define (fast-prime? n times)
+  (define (fermat-test n)
+    (define (try-it a)
+      (define (expmod base exp m)
+        (cond
+          ((= exp 0) 1)
+          ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+          (else (remainder (* base (expmod base (- exp 1) m)) m))
+        )
+      )
+      ; (define (expmod base exp m) (remainder (fast-expt base exp) m))
+      (= (expmod a n n) a)
+    )
+    (try-it (+ 1 (random (- n 1))))
+  )
+  (cond
+    ((= times 0) true)
+    ((fermat-test n) (fast-prime? n (- times 1)))
+    (else false)
+  )
+)
+
+(define (timed-fast-prime-test n)
+  (define (start-prime-test n start-time)
+    (define (report-prime elapsed-time)
+      (display " *** ")
+      (display elapsed-time)
+    )
+    ; (if (prime? n)
+    (when (fast-prime? n 5) ; 試行回数
+      (report-prime (- (runtime) start-time))
+    )
+  )
+  ; (newline)
+  (display n)
+  (start-prime-test n (runtime))
+  (newline)
+)
+
+(define (search-for-fast-primes start end)
+  (define (search-for-fast-primes-iter start end)
+    (when (and (< start end))
+      (and (timed-fast-prime-test start) (search-for-fast-primes-iter (+ start 2) end))
+    )
+  )
+  (search-for-fast-primes-iter (+ start (if (even start) 1 0)) end)
+)
+
+(search-for-fast-primes 1000 1100)
+(search-for-fast-primes 1000000 1000100)
