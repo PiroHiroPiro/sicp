92e72f17bca26e98800859205cc8ff3cf673b304
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-07 14:53:59 +0900

    :+1: done

--- source/1/exercise1.45.rkt
+++ source/1/exercise1.45.rkt
@@ -3,7 +3,6 @@
 (require "../utils/common.rkt")
 
 (define tolerance 0.00001)
-
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
   (define (try guess)
@@ -21,14 +20,35 @@
   (lambda (x) (average x (f x)))
 )
 
-; (define (compose f g)
-;   (lambda (x) (f (g x)))
-; )
-
 (define (repeated f n)
   (if (= n 1)
     (lambda (x) (f x))
-    (lambda (x) (f ((repeated f (- n 1)) x)))
-    ; (compose f (repeated f (- n 1)))
+    (compose f (repeated f (- n 1)))
   )
 )
+
+(define (n-root-test x n c)
+  (fixed-point
+    ((repeated average-damp c) (lambda (y) (/ x (expt y (- n 1)))))
+    1.0
+  )
+)
+
+; (define k 16)
+; (n-root-test (expt 2 k) k 1)
+; (n-root-test (expt 2 k) k 2)
+; (n-root-test (expt 2 k) k 3)
+; (n-root-test (expt 2 k) k 4)
+
+; 1 - 3 -> 1
+; 4 - 7 -> 2
+; 8 - 7 -> 3
+
+(define (n-th-root x n)
+  (fixed-point
+    ((repeated average-damp (round (/ (log n) (log 2)))) (lambda (y) (/ x (expt y (- n 1)))))
+    1.0
+  )
+)
+
+(n-th-root (expt 2 100) 100)
