53fe7eb7c7f4395a977cd738a2f57f3ad748ae90
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-19 00:39:08 +0900

    :+1: 2.20

---
+++ source/2/exercise2.20.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(require "../utils/common.rkt")
+
+(define (reverse l)
+  (define (reverse-iter l r)
+    (if (null? l)
+      r
+      (reverse-iter (cdr l) (cons (car l) r))
+    )
+  )
+  (reverse-iter l '())
+)
+
+(define (same-parity . l)
+  (let
+    ((parity? (if (even? (car l)) even? odd?)))
+    (define (same-parity-iter l spl)
+      (if (null? l)
+        (reverse spl)
+        (same-parity-iter (cdr l) (if (parity? (car l)) (cons (car l) spl) spl))
+      )
+    )
+    (same-parity-iter l '())
+  )
+)
+
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7)
