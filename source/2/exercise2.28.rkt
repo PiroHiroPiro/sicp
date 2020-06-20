098f0b6514e8fe5f4e9b832a5b3afb54e6dec836
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-21 04:28:35 +0900

    :+1: 2.28

--- source/2/exercise2.28.rkt
+++ source/2/exercise2.28.rkt
@@ -3,15 +3,14 @@
 (define x (list (list 1 2) (list 3 4)))
 
 (define (fringe l)
+  ; (and (display l) (newline))
   (cond
-    ((null? l) null)
-    ((not (pair? l)) (list(l)))
-    (else (append (fringe (car l)) (fringe (cdr l))))
+    ((null? l) null)  ; '() -> '()
+    ((not (pair? l)) (list l))  ; 1 -> '(1)
+    ((pair? l) (append (fringe (car l)) (fringe (cdr l)) ))  ; '(1 2) -> (append '(1) '(2))
+    (else l)  ; '(2) -> '(2)
   )
 )
 
 (fringe x)
-; => (1 2 3 4)
-
 (fringe (list x x))
-; => (1 2 3 4 1 2 3 4)
