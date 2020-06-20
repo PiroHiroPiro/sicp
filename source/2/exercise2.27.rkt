e17a2eb54a0b5de856b408d2d5646ca28c02f01c
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-21 01:40:17 +0900

    :+1: 2.27

---
+++ source/2/exercise2.27.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require racket/trace)
+
+(define x (list (list 1 2) (list 3 4)))
+; => ((1 2) (3 4))
+
+(reverse x)
+; => ((3 4) (1 2))
+
+(define (deep-reverse l)
+  (define (deep-reverse-iter l r)
+    (cond
+      ((null? l) r)
+      ((pair? (car l)) (deep-reverse-iter (cdr l) (append (list (deep-reverse (car l))) r)))
+      (else (deep-reverse-iter (cdr l) (append (list (car l)) r)))
+    )
+  )
+  (deep-reverse-iter l null)
+)
+
+(deep-reverse x)
+; => ((4 3) (2 1))
