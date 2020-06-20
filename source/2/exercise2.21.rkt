9e234bd4e8034e43d539e39211d1877522909ab1
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-20 12:35:21 +0900

    :wastebasket: rm map function

--- source/2/exercise2.21.rkt
+++ source/2/exercise2.21.rkt
@@ -9,13 +9,6 @@
   )
 )
 
-(define (map proc items)
-  (if (null? items)
-    nil
-    (cons (proc (car items)) (map proc (cdr items)))
-  )
-)
-
 (define (square-list items)
   (map square items)
 )
