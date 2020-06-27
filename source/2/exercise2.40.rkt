caba4cd6a8d77c756df937368cd7d6ea52f6cfc7
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-28 02:08:56 +0900

    :+1: 2.40

---
+++ source/2/exercise2.40.rkt
@@ -0,0 +1,47 @@
+#lang racket
+
+(require "../utils/common.rkt")
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq))
+)
+
+(define (enumerate-interval low high)
+  (if (> low high)
+    null
+    (cons low (enumerate-interval (+ low 1) high))
+  )
+)
+
+(define (unique-pairs n)
+  (flatmap
+    (lambda (i)
+      (map
+        (lambda (j) (list i j))
+        (enumerate-interval 1 (- i 1))
+      )
+    )
+    (enumerate-interval 1 n)
+  )
+)
+
+(unique-pairs 3)
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair)))
+)
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair)))
+)
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+    (filter
+      prime-sum?
+      (unique-pairs n)
+    )
+  )
+)
+
+(prime-sum-pairs 6)
