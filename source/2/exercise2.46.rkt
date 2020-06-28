b36d1aaa538eec2536d8c52fe072b9cc04d46486
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-28 17:26:17 +0900

    :pencil2: modify 2.46

--- source/2/exercise2.46.rkt
+++ source/2/exercise2.46.rkt
@@ -7,35 +7,13 @@
 (define (ycor-vect vect) (cdr vect))
 
 (define (add-vect vect1 vect2)
-  (let
-    (
-      ((xv1) (xcor-vect vect1))
-      ((yv1) (ycor-vect vect1))
-      ((xv2) (xcor-vect vect2))
-      ((yv1) (ycor-vect vect1))
-    )
-    (cons (+ xv1 xv2) (+ yv1 yv2))
-  )
+  (make-vect (+ (xcor-vect vect1) (xcor-vect vect2)) (+ (ycor-vect vect1) (ycor-vect vect2)))
 )
 
 (define (sub-vect vect1 vect2)
-  (let
-    (
-      ((xv1) (xcor-vect vect1))
-      ((yv1) (ycor-vect vect1))
-      ((xv2) (xcor-vect vect2))
-      ((yv1) (ycor-vect vect1))
-    )
-    (cons (- xv1 xv2) (- yv1 yv2))
-  )
+  (make-vect (- (xcor-vect vect1) (xcor-vect vect2)) (- (ycor-vect vect1) (ycor-vect vect2)))
 )
 
 (define (scale-vect vect s)
-  (let
-    (
-      ((xv) (xcor-vect vect))
-      ((yv) (ycor-vect vect))
-    )
-    (cons (* xv s) (* yv s))
-  )
+  (make-vect (* (xcor-vect vect) s) (* (ycor-vect vect) s))
 )
