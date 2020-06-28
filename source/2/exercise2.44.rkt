b9b0bf6940e4c77a1081f181e81636491e73217a
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-06-28 16:55:15 +0900

    :+1: 2.44

---
+++ source/2/exercise2.44.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let
+      ((smaller (right-split painter (- n 1))))
+      (below painter (beside smaller smaller))
+    )
+  )
+)
