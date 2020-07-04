52352e43847f25ce7ed824eb861fba90591b53d6
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-04 12:48:28 +0900

    :bug: 2.45

--- source/2/exercise2.45.rkt
+++ source/2/exercise2.45.rkt
@@ -1,5 +1,13 @@
 #lang racket
 
 (define (split painter1 painter2)
-  (lambda (image) (painter1 image (painter2 image)))
+  (lambda (image n)
+    (if (= n 0)
+      image
+      (let
+        ((smaller ((split painter1 painter2) image (- n 1))))
+        (painter1 image (painter2 smaller smaller))
+      )
+    )
+  )
 )
